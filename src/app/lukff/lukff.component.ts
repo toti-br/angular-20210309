9d3675cf18b8607bb74967a3f36a1a1a3aea251e
Lukff <dlukff@gmail.com>
2021-03-09 20:10:44 -0300

    2 files modified, 3 added

---
+++ src/app/lukff/lukff.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lukff',
+  templateUrl: './lukff.component.html',
+  styleUrls: ['./lukff.component.css']
+})
+export class LukffComponent implements OnInit {
+
+  user = 'lukff'
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
