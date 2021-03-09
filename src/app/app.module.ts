7ca5cb04bcb672f9ef41e9257b62d1ec99e664c1
julián Boetto <malostratoscba@gmail.com>
2021-03-09 20:25:12 -0300

    Feito componente Julian

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { LukffComponent } from './lukff/lukff.component';
+import { JulianboettoComponent } from './julianboetto/julianboetto.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, LukffComponent ],
+  declarations: [ AppComponent, HelloComponent, LukffComponent, JulianboettoComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
