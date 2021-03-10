4845c1dd2aadadd4013fee9ed2ad6ccc9a472e5a
DiegoCelisT <diegofercelis@gmail.com>
2021-03-09 21:03:26 -0300

    Diego 2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { LukffComponent } from './lukff/lukff.component';
+import { DiegoCelisTComponent } from './diego-celis-t/diego-celis-t.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, LukffComponent ],
+  declarations: [ AppComponent, HelloComponent, LukffComponent, DiegoCelisTComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/diego-celis-t/diego-celis-t.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-diego-celis-t",
+  templateUrl: "./diego-celis-t.component.html",
+  styleUrls: ["./diego-celis-t.component.css"]
+})
+export class DiegoCelisTComponent implements OnInit {
+  userName = "Diego";
+
+  constructor() {}
+
+  ngOnInit() {}
+}
